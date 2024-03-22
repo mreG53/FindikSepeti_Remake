109301ce76bbb9d61c0f11f00c00b4d43fa4fd66
Emre Gümüş <o214001063@giresun.edu.tr>
2024-03-22 22:50:13 +0300

    tailwind

--- obj/Debug/net6.0/FindikSepeti_Remake.AssemblyInfo.cs
+++ obj/Debug/net6.0/FindikSepeti_Remake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FindikSepeti_Remake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc19cdb7bc6f93b533fc3673021601b44a641b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6811dcda6212b8f558f00cd26502aa8774e12a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FindikSepeti_Remake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FindikSepeti_Remake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
