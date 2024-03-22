ad0ea7ff0744c4661665b75ed1209b0b9397c8f0
Emre Gümüş <o214001063@giresun.edu.tr>
2024-03-22 20:16:28 +0300

    after initialize

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FindikSepeti_Remake.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
