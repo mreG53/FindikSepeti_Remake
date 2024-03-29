623df1d47085f2085930ef5fd87ca04ff3862a67
ErenB1ber <149535575+ErenB1ber@users.noreply.github.com>
2024-03-29 17:01:29 +0300

    efekt düzeltildi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,39 +10,3 @@
         }
     });
 });
-window.addEventListener('load', () => {
-    // 3 saniye sonra çalışacak olan kodu setTimeout ile belirtiyoruz
-    setTimeout(() => {
-        // Sayfa yüklendiğinde navbar'a 'navbar-loaded' sınıfını ekle
-        document.querySelector('.navbar').classList.add('navbar-loaded');
-    }, 1000);
-    setTimeout(() => {
-        // Sayfa yüklendiğinde navbar'a 'navbar-loaded' sınıfını ekle
-        document.querySelector('.navbar').classList.add('lg:w-5/6', 'w-3/4');
-    }, 5000);
-    var txtOpaElement = document.querySelectorAll('.navbar .txtOpa');
-
-    // Belirli bir süre sonra display özelliğini değiştirin
-    var txtOpaElements = document.querySelectorAll('.navbar .txtOpa');
-
-    // Her bir öğe için işlem yapın
-    txtOpaElements.forEach(function (txtOpaElement) {
-        // Belirli bir süre sonra display özelliğini değiştirin
-        setTimeout(function () {
-            txtOpaElement.style.display = 'flex';
-        }, 5200);
-    });
-    var otherContentElements = document.querySelectorAll('.other-content');
-
-    // Her bir elemente 'show' sınıfını ekleyin
-    otherContentElements.forEach(function (element) {
-        element.classList.add('show');
-    });
-    setTimeout(function () {
-        // Elementi seçin
-        var myElement = document.getElementById('arkaplan');
-
-        // Arka plan rengini değiştirin
-        myElement.style.backgroundColor = 'rgb(255, 237, 213)';
-    }, 5000);
-});
